702f6c2d68785ea345844eade49069406546bda1
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-04-17 13:17:07 +0300

    IntroductionToHibernateJPA

---
+++ w05IntroductionToHibernateJPA/src/main/java/Main.java
@@ -0,0 +1,30 @@
+import entities.Teacher;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("general");
+
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+
+//        Teacher teacher = new Teacher();
+//        teacher.setName("Geri");
+//        entityManager.persist(teacher);
+
+        Teacher fromDB = entityManager.find(Teacher.class, 3);
+        System.out.println(fromDB);
+
+        entityManager.getTransaction().commit();
+
+
+
+
+
+    }
+}
