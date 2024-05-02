7e0520891c0fbc5748b66ebed5250404d2015a60
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-05-02 17:06:34 +0300

    w06ExerciseIntroductionToHibernate

---
+++ w06ExerciseIntroductionToHibernate/src/main/java/P10IncreaseSalaries.java
@@ -0,0 +1,31 @@
+import entities.Employee;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class P10IncreaseSalaries {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("PU_Name");
+
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        List<Employee> resultList = entityManager.createQuery("SELECT e FROM Employee e JOIN e.department d " +
+                        "WHERE d.name IN('Engineering', 'Tool Design', 'Marketing', 'Information Services')", Employee.class)
+                .getResultList();
+
+        for (Employee employee : resultList) {
+            employee.setSalary(employee.getSalary().multiply(BigDecimal.valueOf(1.12)));
+            entityManager.persist(employee);
+            System.out.printf("%s %s ($%.2f)%n", employee.getFirstName(), employee.getLastName(), employee.getSalary());
+        }
+
+        entityManager.getTransaction().commit();
+
+    }
+}
