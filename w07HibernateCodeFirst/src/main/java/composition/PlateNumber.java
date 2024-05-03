8552b6b30e32b51c9a79f77e72450cbe36c3c132
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-05-03 18:19:54 +0300

    w07HibernateCodeFirst

---
+++ w07HibernateCodeFirst/src/main/java/composition/PlateNumber.java
@@ -0,0 +1,19 @@
+package composition;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "plate_numbers")
+public class PlateNumber {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Basic
+    private String number;
+
+    public PlateNumber(){}
+
+    public PlateNumber(String number) {
+        this.number = number;
+    }
+}
