cd1b9da3f643abe19080ae7801229fd0cc296ea5
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-05-03 18:19:45 +0300

    w07HibernateCodeFirst

---
+++ w07HibernateCodeFirst/src/main/java/composition/Driver.java
@@ -0,0 +1,21 @@
+package composition;
+
+import inheritance.entities.Truck;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "drivers")
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "full_name")
+    private String fullNma;
+
+    @ManyToMany(mappedBy = "drivers")
+    private List<Truck> trucks;
+
+    public Driver() {}
+}
