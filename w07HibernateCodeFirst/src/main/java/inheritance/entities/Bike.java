68abd75b1fbc5a4b9988e7b243aaf833b7391cb8
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-05-04 20:07:45 +0300

    w07HibernateCodeFirst

---
+++ w07HibernateCodeFirst/src/main/java/inheritance/entities/Bike.java
@@ -0,0 +1,17 @@
+package inheritance.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "bikes")
+public class Bike extends Vehicle{
+    private static final String BIKE_TYPE = "BIKE";
+    public Bike() {}
+
+    public Bike(String model, BigDecimal price, String fuelType) {
+        super(BIKE_TYPE, model, price, fuelType);
+    }
+}
