5eaa70b1a0687a1c7b037d2927c77cd858fcf91a
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-05-05 14:56:45 +0300

    w07HibernateCodeFirst

---
+++ w07HibernateCodeFirst/src/main/java/inheritance/entities/Car.java
@@ -0,0 +1,29 @@
+package inheritance.entities;
+
+import composition.PlateNumber;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+import java.math.BigDecimal;
+
+@Entity
+
+public class Car extends Vehicle{
+    private static final String CAR_TYPE = "CAR";
+    @Basic
+    private int seats;
+    @OneToOne
+    @JoinColumn(name = "plate_number_id", referencedColumnName = "id")
+    private PlateNumber plateNumber;
+
+    public Car() {
+    }
+
+    public Car(String model, BigDecimal price, String fuelType, int seats, PlateNumber plateNumber) {
+        super(CAR_TYPE, model, price, fuelType);
+        this.seats = seats;
+        this.plateNumber = plateNumber;
+    }
+}
