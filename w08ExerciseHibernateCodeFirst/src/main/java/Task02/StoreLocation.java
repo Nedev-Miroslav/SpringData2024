d3a4cb7c78a39610d8a98788422146b1ad413b89
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-05-07 13:06:23 +0300

    w08ExerciseHibernateCodeFirst

---
+++ w08ExerciseHibernateCodeFirst/src/main/java/Task02/StoreLocation.java
@@ -0,0 +1,49 @@
+package Task02;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "store_location")
+public class StoreLocation {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "location_name", nullable = false)
+    private String locationName;
+    @OneToMany(targetEntity = Sale.class, mappedBy = "storeLocation")
+    private Set<Sale> sales;
+
+    public Set<Sale> getSales() {
+        return sales;
+    }
+
+    public void setSales(Set<Sale> sales) {
+        this.sales = sales;
+    }
+
+    public StoreLocation() {
+        this.sales = new HashSet<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+}
