39166eec1d20bd266ad6f57884dd667bb49b7c7f
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-05-12 15:38:36 +0300

    w09SpringDataIntro

---
+++ w09SpringDataIntro/src/main/java/com/example/w09springdataintro/W09SpringDataIntroApplication.java
@@ -0,0 +1,13 @@
+package com.example.w09springdataintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W09SpringDataIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W09SpringDataIntroApplication.class, args);
+    }
+
+}
