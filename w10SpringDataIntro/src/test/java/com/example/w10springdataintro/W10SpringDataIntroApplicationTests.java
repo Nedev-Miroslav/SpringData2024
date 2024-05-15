8b19ff28aaf2c3cc63fd88e35ec965c60676496f
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-05-15 09:43:59 +0300

    w10SpringDataIntro

---
+++ w10SpringDataIntro/src/test/java/com/example/w10springdataintro/W10SpringDataIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.w10springdataintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W10SpringDataIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
