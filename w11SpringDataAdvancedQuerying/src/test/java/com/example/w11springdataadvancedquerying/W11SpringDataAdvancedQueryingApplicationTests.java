05e783b536c3c92acdbd5b4b9dd5236ef0fe4bab
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-05-20 11:27:38 +0300

    w11SpringDataAdvancedQuerying

---
+++ w11SpringDataAdvancedQuerying/src/test/java/com/example/w11springdataadvancedquerying/W11SpringDataAdvancedQueryingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.w11springdataadvancedquerying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W11SpringDataAdvancedQueryingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
