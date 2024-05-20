809a16a70c4f92873fb887fb9f54b89335b074ca
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-05-20 11:28:09 +0300

    w11SpringDataAdvancedQuerying

---
+++ w11SpringDataAdvancedQuerying/src/main/java/com/example/w11springdataadvancedquerying/W11SpringDataAdvancedQueryingApplication.java
@@ -0,0 +1,13 @@
+package com.example.w11springdataadvancedquerying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W11SpringDataAdvancedQueryingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W11SpringDataAdvancedQueryingApplication.class, args);
+    }
+
+}
