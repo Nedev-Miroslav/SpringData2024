505ac136a97b3ee86435d161e963adbe06d983ec
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-05-24 11:16:27 +0300

    w12exercisespringdataadvancedquerying

---
+++ w12ExerciseSpringDataAdvancedQuerying/src/main/java/com/example/w12exercisespringdataadvancedquerying/W12ExerciseSpringDataAdvancedQueryingApplication.java
@@ -0,0 +1,13 @@
+package com.example.w12exercisespringdataadvancedquerying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W12ExerciseSpringDataAdvancedQueryingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W12ExerciseSpringDataAdvancedQueryingApplication.class, args);
+    }
+
+}
