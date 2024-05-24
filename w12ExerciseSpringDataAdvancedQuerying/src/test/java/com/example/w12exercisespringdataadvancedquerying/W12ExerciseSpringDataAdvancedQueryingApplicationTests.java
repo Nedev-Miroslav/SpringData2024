f06f35bb7d3b6a84962954eab8ca73521be286e9
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-05-24 11:14:55 +0300

    w12ExerciseSpringDataAdvancedQuerying

---
+++ w12ExerciseSpringDataAdvancedQuerying/src/test/java/com/example/w12exercisespringdataadvancedquerying/W12ExerciseSpringDataAdvancedQueryingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.w12exercisespringdataadvancedquerying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W12ExerciseSpringDataAdvancedQueryingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
