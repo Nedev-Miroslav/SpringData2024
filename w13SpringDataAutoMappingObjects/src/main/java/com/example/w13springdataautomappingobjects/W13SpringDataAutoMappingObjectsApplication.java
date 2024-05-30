bd6dd744de4fba76a477f37d15a4bc4ba1a9f524
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-05-30 16:04:03 +0300

    w13SpringDataAutoMappingObjects

---
+++ w13SpringDataAutoMappingObjects/src/main/java/com/example/w13springdataautomappingobjects/W13SpringDataAutoMappingObjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.w13springdataautomappingobjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W13SpringDataAutoMappingObjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W13SpringDataAutoMappingObjectsApplication.class, args);
+    }
+
+}
