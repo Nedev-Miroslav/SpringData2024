fb3f53ea6bda8dd76fbb8ec75a526df6af527090
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-05-31 13:14:42 +0300

    w13SpringDataAutoMappingObjects

---
+++ w13SpringDataAutoMappingObjects/src/main/java/com/example/w13springdataautomappingobjects/models/Employee.java
@@ -0,0 +1,89 @@
+package com.example.w13springdataautomappingobjects.models;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private BigDecimal salary;
+
+    private LocalDate birthDay;
+
+    @ManyToOne
+    private  Address address;
+
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, BigDecimal salary, LocalDate birthDay, Address address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.birthDay = birthDay;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay = birthDay;
+    }
+}
