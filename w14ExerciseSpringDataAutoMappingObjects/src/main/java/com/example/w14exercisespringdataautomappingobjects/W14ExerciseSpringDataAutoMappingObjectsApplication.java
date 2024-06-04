888f78a7482771018f955400ca8e53f355196256
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-06-04 10:50:24 +0300

    w14exercisespringdataautomappingobjects

---
+++ w14ExerciseSpringDataAutoMappingObjects/src/main/java/com/example/w14exercisespringdataautomappingobjects/W14ExerciseSpringDataAutoMappingObjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.w14exercisespringdataautomappingobjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W14ExerciseSpringDataAutoMappingObjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W14ExerciseSpringDataAutoMappingObjectsApplication.class, args);
+    }
+
+}
