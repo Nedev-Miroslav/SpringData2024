c8e10b92be0195766ceabbd8ba503f46ddde65c3
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-06-04 10:49:31 +0300

    w14ExerciseSpringDataAutoMappingObjects

---
+++ w14ExerciseSpringDataAutoMappingObjects/src/test/java/com/example/w14exercisespringdataautomappingobjects/W14ExerciseSpringDataAutoMappingObjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.w14exercisespringdataautomappingobjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W14ExerciseSpringDataAutoMappingObjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
