1fcdfcaacebad7620b51dc1437b067703fdee66f
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-06-05 10:49:00 +0300

    w14ExerciseSpringDataAutoMappingObjects

---
+++ w14ExerciseSpringDataAutoMappingObjects/src/main/java/com/example/w14exercisespringdataautomappingobjects/util/ValidatorServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.w14exercisespringdataautomappingobjects.util;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class ValidatorServiceImpl implements ValidatorService {
+
+    private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();;
+
+
+    @Override
+    public <E> boolean isValid(E entity) {
+        return this.validator.validate(entity).isEmpty();
+    }
+
+    @Override
+    public <E> Set<ConstraintViolation<E>> validate(E entity) {
+        return this.validator.validate(entity);
+    }
+}
