9b2a1f94be483bf49a869f23f8e4e45f38c3ccc8
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-06-06 11:04:17 +0300

    w14exercisespringdataautomappingobjects

---
+++ w14ExerciseSpringDataAutoMappingObjects/src/main/java/com/example/w14exercisespringdataautomappingobjects/util/data/entities/Order.java
@@ -0,0 +1,19 @@
+package com.example.w14exercisespringdataautomappingobjects.data.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "orders")
+public class Order extends BaseEntity {
+
+    private LocalDateTime time;
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+    @ManyToMany
+    private Set<Game> games;
+
+}
