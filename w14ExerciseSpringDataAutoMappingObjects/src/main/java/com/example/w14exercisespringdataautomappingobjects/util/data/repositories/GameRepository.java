cb3b354e460d0a298e5dd8a71c855f2fecc5639e
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-06-07 10:36:19 +0300

    w14exercisespringdataautomappingobjects

---
+++ w14ExerciseSpringDataAutoMappingObjects/src/main/java/com/example/w14exercisespringdataautomappingobjects/util/data/repositories/GameRepository.java
@@ -0,0 +1,14 @@
+package com.example.w14exercisespringdataautomappingobjects.data.repositories;
+
+import com.example.w14exercisespringdataautomappingobjects.data.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+
+    Optional<Game> findByTitle(String title);
+}
