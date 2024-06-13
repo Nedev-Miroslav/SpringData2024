2cb951913087cbf7cf83ad17d925ffe0aaca23c5
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-06-13 12:08:50 +0300

    w15JSONProcessing

---
+++ w15JSONProcessing/src/test/java/com/example/w15jsonprocessing/W15JsonProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.w15jsonprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W15JsonProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
