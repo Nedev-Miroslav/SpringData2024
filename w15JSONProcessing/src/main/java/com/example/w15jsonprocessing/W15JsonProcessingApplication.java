35555780873d29ecdf8622dc89f6efa63adf42df
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-06-13 12:09:36 +0300

    w15jsonprocessing

---
+++ w15JSONProcessing/src/main/java/com/example/w15jsonprocessing/W15JsonProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.w15jsonprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W15JsonProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W15JsonProcessingApplication.class, args);
+    }
+
+}
