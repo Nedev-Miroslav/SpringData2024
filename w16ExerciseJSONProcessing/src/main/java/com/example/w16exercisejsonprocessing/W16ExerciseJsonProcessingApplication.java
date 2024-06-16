9cecb41fb847748a0f7f74b24785032513918819
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-06-16 10:30:06 +0300

    w16ExerciseJSONProcessing

---
+++ w16ExerciseJSONProcessing/src/main/java/com/example/w16exercisejsonprocessing/W16ExerciseJsonProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.w16exercisejsonprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W16ExerciseJsonProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W16ExerciseJsonProcessingApplication.class, args);
+    }
+
+}
