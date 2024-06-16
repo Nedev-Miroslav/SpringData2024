26bc25b8be84c461f91ec69236118fa83763355d
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-06-16 10:29:19 +0300

    w16ExerciseJSONProcessing

---
+++ w16ExerciseJSONProcessing/src/test/java/com/example/w16exercisejsonprocessing/W16ExerciseJsonProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.w16exercisejsonprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W16ExerciseJsonProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
