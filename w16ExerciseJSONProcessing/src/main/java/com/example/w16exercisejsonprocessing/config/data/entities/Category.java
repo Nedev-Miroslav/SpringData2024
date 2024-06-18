0297d0165ac81a2eba159b689f72078a905c546d
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-06-18 10:48:19 +0300

    w16ExerciseJSONProcessing

---
+++ w16ExerciseJSONProcessing/src/main/java/com/example/w16exercisejsonprocessing/config/data/entities/Category.java
@@ -0,0 +1,45 @@
+package com.example.w16exercisejsonprocessing.data.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+public class Category extends BaseEntity{
+
+    @Column(nullable = false, unique = true)
+    private String name;
+    @ManyToMany(mappedBy = "categories", fetch = FetchType.EAGER)
+    private Set<Product> products;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+}
