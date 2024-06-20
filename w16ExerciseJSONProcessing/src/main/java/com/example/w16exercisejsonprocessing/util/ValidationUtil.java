73731f6652b471eab0d4c6e23a450701e1f8ed9b
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-06-20 10:15:22 +0300

    w16exercisejsonprocessing

---
+++ w16ExerciseJSONProcessing/src/main/java/com/example/w16exercisejsonprocessing/util/ValidationUtil.java
@@ -0,0 +1,14 @@
+package com.example.w16exercisejsonprocessing.util;
+
+import jakarta.validation.ConstraintViolation;
+
+import java.util.Set;
+
+public interface ValidationUtil {
+
+    <E> boolean isValid(E entity);
+
+    <E> Set<ConstraintViolation<E>> getViolations(E entity);
+
+
+}
