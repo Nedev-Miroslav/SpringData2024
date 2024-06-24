d9fc2c744ee7ff3bd2244ea36f26d5fd60b19d9a
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-06-24 11:42:08 +0300

    w16ExerciseJSONProcessing

---
+++ w16ExerciseJSONProcessing/src/main/java/com/example/w16exercisejsonprocessing/service/dtos/export/ProductInfoDto.java
@@ -0,0 +1,31 @@
+package com.example.w16exercisejsonprocessing.service.dtos.export;
+
+
+import com.google.gson.annotations.Expose;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class ProductInfoDto implements Serializable {
+
+    @Expose
+    private String name;
+    @Expose
+    private BigDecimal price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
