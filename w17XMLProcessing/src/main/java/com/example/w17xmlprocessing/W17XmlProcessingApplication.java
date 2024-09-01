46837baeb7b15cab1d8fbfeff4ba840c48f7cf69
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-09-01 17:14:45 +0300

    xmlprocessing

---
+++ w17XMLProcessing/src/main/java/com/example/w17xmlprocessing/W17XmlProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.w17xmlprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W17XmlProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W17XmlProcessingApplication.class, args);
+    }
+
+}
