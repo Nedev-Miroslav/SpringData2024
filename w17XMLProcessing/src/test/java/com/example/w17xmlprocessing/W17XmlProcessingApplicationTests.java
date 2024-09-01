4830509f2a41a7866bfd8de1babe7783191c0c21
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-09-01 17:14:00 +0300

    XMLProcessing

---
+++ w17XMLProcessing/src/test/java/com/example/w17xmlprocessing/W17XmlProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.w17xmlprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W17XmlProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
