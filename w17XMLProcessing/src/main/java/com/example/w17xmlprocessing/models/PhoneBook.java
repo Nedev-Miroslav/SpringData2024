cf7938d03a2a748724c4da2120f9a6f88ff9a1b1
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-09-03 10:55:09 +0300

    add models

---
+++ w17XMLProcessing/src/main/java/com/example/w17xmlprocessing/models/PhoneBook.java
@@ -0,0 +1,36 @@
+package com.example.w17xmlprocessing.models;
+
+import jakarta.xml.bind.annotation.*;
+
+import java.util.List;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PhoneBook {
+
+    private String owner;
+    @XmlElementWrapper(name = "numbers-wrapper")
+    @XmlElement(name = "numbers-item")
+    private List<String> numbers;
+
+    @XmlElementWrapper
+    @XmlElement(name = "phoneNumber")
+    private List<PhoneNumber> phoneNumbers;
+
+    public PhoneBook() {
+    }
+
+    public PhoneBook(String owner, List<String> numbers, List<PhoneNumber> phoneNumbers) {
+        this.owner = owner;
+        this.numbers = numbers;
+        this.phoneNumbers = phoneNumbers;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
