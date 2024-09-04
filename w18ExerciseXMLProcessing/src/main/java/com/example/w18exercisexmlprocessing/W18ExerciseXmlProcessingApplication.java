a3ffd00d0c1c08632315bc4a8673b3b323796db7
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-09-04 14:08:11 +0300

    ExerciseXMLProcessing

---
+++ w18ExerciseXMLProcessing/src/main/java/com/example/w18exercisexmlprocessing/W18ExerciseXmlProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.w18exercisexmlprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W18ExerciseXmlProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W18ExerciseXmlProcessingApplication.class, args);
+    }
+
+}
