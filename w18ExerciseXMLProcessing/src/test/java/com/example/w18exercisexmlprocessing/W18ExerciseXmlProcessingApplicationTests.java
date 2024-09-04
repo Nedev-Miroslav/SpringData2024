4ba85b4398ea38f41d26f36520d7938fe1b851a4
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-09-04 14:07:53 +0300

    ExerciseXMLProcessing

---
+++ w18ExerciseXMLProcessing/src/test/java/com/example/w18exercisexmlprocessing/W18ExerciseXmlProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.w18exercisexmlprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W18ExerciseXmlProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
