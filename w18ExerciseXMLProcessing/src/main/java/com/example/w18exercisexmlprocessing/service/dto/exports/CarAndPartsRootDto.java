77302f39d30fb6fc3232270df6f3ccdec3d9ae86
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-09-27 11:04:31 +0300

    add CarAndPartsRootDto

---
+++ w18ExerciseXMLProcessing/src/main/java/com/example/w18exercisexmlprocessing/service/dto/exports/CarAndPartsRootDto.java
@@ -0,0 +1,25 @@
+package com.example.w18exercisexmlprocessing.service.dto.exports;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+import java.io.Serializable;
+import java.util.List;
+
+@XmlRootElement(name = "cars")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CarAndPartsRootDto implements Serializable {
+
+    @XmlElement(name = "car")
+    private List<CarAndPartsDto> carAndPartsDtoList;
+
+    public List<CarAndPartsDto> getCarAndPartsDtoList() {
+        return carAndPartsDtoList;
+    }
+
+    public void setCarAndPartsDtoList(List<CarAndPartsDto> carAndPartsDtoList) {
+        this.carAndPartsDtoList = carAndPartsDtoList;
+    }
+}
