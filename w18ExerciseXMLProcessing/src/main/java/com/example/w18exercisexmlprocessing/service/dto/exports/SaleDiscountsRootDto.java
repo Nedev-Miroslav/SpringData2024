e1c605b9229b18be6241d19251c0bca70d2dbdf1
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-09-30 11:16:00 +0300

    add SaleDiscountsRootDto

---
+++ w18ExerciseXMLProcessing/src/main/java/com/example/w18exercisexmlprocessing/service/dto/exports/SaleDiscountsRootDto.java
@@ -0,0 +1,24 @@
+package com.example.w18exercisexmlprocessing.service.dto.exports;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+import java.io.Serializable;
+import java.util.List;
+
+@XmlRootElement(name = "sales")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class SaleDiscountsRootDto implements Serializable {
+
+    @XmlElement(name = "sale")
+    private List<SaleDiscountDto> saleDiscountDtos;
+
+    public List<SaleDiscountDto> getSaleDiscountDtos() {
+        return saleDiscountDtos;
+    }
+
+    public void setSaleDiscountDtos(List<SaleDiscountDto> saleDiscountDtos) {
+        this.saleDiscountDtos = saleDiscountDtos;
+    }
+}
