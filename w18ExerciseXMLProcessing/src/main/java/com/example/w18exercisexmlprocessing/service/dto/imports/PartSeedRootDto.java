8ab1f59998bd967b4225839f62c2524ec92f2fa9
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-10-10 20:56:25 +0300

    add PartSeedRootDto

---
+++ w18ExerciseXMLProcessing/src/main/java/com/example/w18exercisexmlprocessing/service/dto/imports/PartSeedRootDto.java
@@ -0,0 +1,26 @@
+package com.example.w18exercisexmlprocessing.service.dto.imports;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+import java.io.Serializable;
+import java.util.List;
+
+@XmlRootElement(name = "parts")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PartSeedRootDto implements Serializable {
+
+    @XmlElement(name = "part")
+    private List<PartSeedDto> partSeedDtoList;
+
+
+    public List<PartSeedDto> getPartSeedDtoList() {
+        return partSeedDtoList;
+    }
+
+    public void setPartSeedDtoList(List<PartSeedDto> partSeedDtoList) {
+        this.partSeedDtoList = partSeedDtoList;
+    }
+}
