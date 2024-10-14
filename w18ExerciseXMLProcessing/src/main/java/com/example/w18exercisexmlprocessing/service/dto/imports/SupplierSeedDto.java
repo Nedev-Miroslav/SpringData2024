e8b30b5098b21a83d1dc4db0e0e2345eb55ad21f
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-10-14 20:43:54 +0300

    add SupplierSeedDto

---
+++ w18ExerciseXMLProcessing/src/main/java/com/example/w18exercisexmlprocessing/service/dto/imports/SupplierSeedDto.java
@@ -0,0 +1,35 @@
+package com.example.w18exercisexmlprocessing.service.dto.imports;
+
+import jakarta.validation.constraints.Size;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "supplier")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class SupplierSeedDto {
+
+    @XmlAttribute
+    @Size(min = 5)
+    private String name;
+
+    @XmlAttribute(name = "is-importer")
+    private boolean isImporter;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isImporter() {
+        return isImporter;
+    }
+
+    public void setImporter(boolean importer) {
+        isImporter = importer;
+    }
+}
