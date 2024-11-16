6f04b002f058300c222cdc8ec0707cf88efcfb59
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-11-16 12:40:02 +0200

    add MemberDto

---
+++ PractisOne/skeleton/src/main/java/softuni/exam/models/dto/xmls/MemberDto.java
@@ -0,0 +1,22 @@
+package softuni.exam.models.dto.xmls;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement(name = "member")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class MemberDto implements Serializable {
+    @XmlElement
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
