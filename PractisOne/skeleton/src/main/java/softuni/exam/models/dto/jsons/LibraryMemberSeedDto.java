1e3223da1b58eac088653455dce8144c3117650b
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-11-16 12:38:40 +0200

    add LibraryMemberSeedDto

---
+++ PractisOne/skeleton/src/main/java/softuni/exam/models/dto/jsons/LibraryMemberSeedDto.java
@@ -0,0 +1,63 @@
+package softuni.exam.models.dto.jsons;
+
+import com.google.gson.annotations.Expose;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+public class LibraryMemberSeedDto implements Serializable {
+    @Expose
+    @Size(min = 2, max = 40)
+    private String address;
+
+    @Expose
+    @NotNull
+    @Size(min = 2, max = 30)
+    private String firstName;
+
+    @Expose
+    @NotNull
+    @Size(min = 2, max = 30)
+    private String lastName;
+
+    @Expose
+    @NotNull
+    @Size(min = 2, max = 20)
+    private String phoneNumber;
+
+
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
