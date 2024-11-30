f4b1315816eb80e4a1cfe62a327dc2f3403bf7fd
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-11-30 11:20:23 +0200

    add Local Date Adaptor

---
+++ PractisTwo/skeleton/src/main/java/softuni/exam/util/LocalDateAdaptor.java
@@ -0,0 +1,19 @@
+package softuni.exam.util;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateAdaptor extends XmlAdapter<String, LocalDate> {
+    private final DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private final DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Override
+    public LocalDate unmarshal(String s) throws Exception {
+        return LocalDate.parse(s, inputFormatter);
+    }
+
+    @Override
+    public String marshal(LocalDate localDate) throws Exception {
+        return localDate.format(outputFormatter);
+    }
+}
