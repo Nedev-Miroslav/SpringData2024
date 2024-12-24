d43c64220dfd8641b3c3dc3e654595dfe98a208b
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-12-24 11:34:28 +0200

    add AgentDto

---
+++ PractisTwo/skeleton/src/main/java/softuni/exam/models/dto/xmls/AgentDto.java
@@ -0,0 +1,25 @@
+package softuni.exam.models.dto.xmls;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement(name = "agent")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class AgentDto implements Serializable {
+    public AgentDto() {
+    }
+
+    @XmlElement
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
