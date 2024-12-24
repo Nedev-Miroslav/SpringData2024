e80f1199269147c5a04801cbd1d0feaa3236e193
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2024-12-24 11:36:00 +0200

    add ApartmentSeedDto

---
+++ PractisTwo/skeleton/src/main/java/softuni/exam/models/dto/xmls/ApartmentSeedDto.java
@@ -0,0 +1,47 @@
+package softuni.exam.models.dto.xmls;
+
+import javax.validation.constraints.DecimalMin;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement(name = "apartment")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ApartmentSeedDto implements Serializable {
+
+    @XmlElement
+    private String apartmentType;
+
+    @XmlElement
+    @DecimalMin(value = "40.00")
+    private Double area;
+
+    @XmlElement
+    private String town;
+
+    public String getApartmentType() {
+        return apartmentType;
+    }
+
+    public void setApartmentType(String apartmentType) {
+        this.apartmentType = apartmentType;
+    }
+
+    public Double getArea() {
+        return area;
+    }
+
+    public void setArea(Double area) {
+        this.area = area;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+}
