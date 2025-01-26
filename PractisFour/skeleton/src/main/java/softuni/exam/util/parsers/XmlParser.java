cee763249aafb8199fe24f58ea93a3ce11049935
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-01-26 11:49:19 +0200

    add XmlParser

---
+++ PractisFour/skeleton/src/main/java/softuni/exam/util/parsers/XmlParser.java
@@ -0,0 +1,20 @@
+package softuni.exam.util.parsers;
+
+
+import javax.xml.bind.JAXBException;
+
+public interface XmlParser {
+
+    <E> E parse(Class<E> clazz, String path) throws JAXBException;
+
+
+    <E> void exportToFile(Class<E> clazz, E object, String path) throws JAXBException;
+
+
+
+
+//    <E> void exportToFile(E object, String path) throws JAXBException;
+
+//   Не е необходимо да подаваме класът на обекта можем да вземем кой е
+//   класът при извикване с Object.getClass
+}
