15ce24d92a4443ddd591d845f02f4b294655b6fd
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-03-07 19:27:05 +0200

    add Person entity

---
+++ PractisFour/skeleton/src/main/java/softuni/exam/models/entity/Person.java
@@ -0,0 +1,76 @@
+package softuni.exam.models.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "persons")
+public class Person extends BaseEntity{
+
+    @Column(name = "first_name", nullable = false, unique = true)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String phone;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "status_type", nullable = false)
+    private StatusType statusType;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id", referencedColumnName = "id")
+    private Country country;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public StatusType getStatusType() {
+        return statusType;
+    }
+
+    public void setStatusType(StatusType statusType) {
+        this.statusType = statusType;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+}
