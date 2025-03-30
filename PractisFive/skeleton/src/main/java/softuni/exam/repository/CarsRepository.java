1412437b9cc36587d2da343aeaa7b06a92a9f26c
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-03-30 14:27:39 +0300

    add CarsRepository

---
+++ PractisFive/skeleton/src/main/java/softuni/exam/repository/CarsRepository.java
@@ -0,0 +1,16 @@
+package softuni.exam.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import softuni.exam.models.dto.xmls.CarSeedDto;
+import softuni.exam.models.entity.Car;
+
+import java.util.Optional;
+
+@Repository
+public interface CarsRepository extends JpaRepository<Car, Long> {
+
+
+    Optional<Car> getByPlateNumber(String plateNumber);
+
+}
