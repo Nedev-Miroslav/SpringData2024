15125bd04ad4d368e7d233d98eae05d70c725754
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-04-05 11:23:06 +0300

    add Validations

---
+++ PractisSix/skeleton/src/main/java/com/example/football/util/Validations/ValidationUtil.java
@@ -0,0 +1,8 @@
+package com.example.football.util.Validations;
+
+public interface ValidationUtil {
+
+    <E> boolean isValid(E e);
+
+
+}
---
+++ PractisSix/skeleton/src/main/java/com/example/football/util/Validations/ValidationUtilImpl.java
@@ -0,0 +1,20 @@
+package com.example.football.util.Validations;
+
+import org.springframework.stereotype.Component;
+
+import javax.validation.Validator;
+
+@Component
+public class ValidationUtilImpl implements ValidationUtil {
+
+    private final Validator validator;
+
+    public ValidationUtilImpl(Validator validator) {
+        this.validator = validator;
+    }
+
+    @Override
+    public <E> boolean isValid(E e) {
+        return this.validator.validate(e).isEmpty();
+    }
+}
