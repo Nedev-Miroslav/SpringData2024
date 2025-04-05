b4503bc24ccb4e84c94e6ccbf54d83c033751462
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-04-05 11:22:49 +0300

    add xmlParser

---
+++ PractisSix/skeleton/src/main/java/com/example/football/util/xmlParser/XmlParser.java
@@ -0,0 +1,20 @@
+package com.example.football.util.xmlParser;
+
+
+import javax.xml.bind.JAXBException;
+
+public interface XmlParser {
+
+    <E> E parse(Class<E> clazz, String path) throws JAXBException;
+
+
+    <E> void exportToFile(Class<E> clazz, E object, String path) throws JAXBException;
+
+
+
+
+//    <E> void exportToFile(E object, String path) throws JAXBException;
+
+//   Не е необходимо да подаваме класът на обекта можем да вземем кой е
+//   класът при извикване с Object.getClass
+}
