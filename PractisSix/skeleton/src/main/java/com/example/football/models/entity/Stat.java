348ce86c19f36faa65c6ab5f1658e70ce3e5cd83
Miroslav Nedev <117746535+Nedev-Miroslav@users.noreply.github.com>
2025-04-06 12:29:09 +0300

    add stat entity

---
+++ PractisSix/skeleton/src/main/java/com/example/football/models/entity/Stat.java
@@ -0,0 +1,56 @@
+package com.example.football.models.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "stats")
+public class Stat extends BaseEntity{
+
+    @Column(nullable = false)
+    private double shooting;
+
+    @Column(nullable = false)
+    private double passing;
+
+    @Column(nullable = false)
+    private double endurance;
+
+    @OneToMany(mappedBy = "stat")
+    private Set<Player> players;
+
+    public double getShooting() {
+        return shooting;
+    }
+
+    public void setShooting(double shooting) {
+        this.shooting = shooting;
+    }
+
+    public double getPassing() {
+        return passing;
+    }
+
+    public void setPassing(double passing) {
+        this.passing = passing;
+    }
+
+    public double getEndurance() {
+        return endurance;
+    }
+
+    public void setEndurance(double endurance) {
+        this.endurance = endurance;
+    }
+
+    public Set<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Set<Player> players) {
+        this.players = players;
+    }
+}
